5963b258244d187b254423bf882123c2c7c8a402
Olga-Melnyk <3717129@ukr.net>
2023-01-19 17:23:25 +0200

    add libraries

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+console.log(SimpleLightbox);
