85eae7492264062b8e5afcf905d8609942e02009
Olga-Melnyk <3717129@ukr.net>
2023-01-20 15:01:47 +0200

    test

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log('hello');
+
+// 32972281-0765619f1e89c459ff8e86751
+
+let a;
+a = 10;
+console.log(a);
